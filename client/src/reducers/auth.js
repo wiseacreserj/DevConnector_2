99d86babd791986c30fe8353445a82984a20f880
Sergey Zadorozhniy <wiseacreserj@gmail.com>
2019-08-20 17:58:42 +0300

    logout link added

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,44 +1,52 @@
-import {REGISTER_SUCCES, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCES, LOGIN_FAIL} from "../../src/actions/types";
+import {
+  REGISTER_SUCCES,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCES,
+  LOGIN_FAIL,
+  LOGOUT
+} from "../../src/actions/types";
 
 const initialState = {
-    token: localStorage.getItem("token"),
-    isAuthenticated: null,
-    loading: true,
-    user: null
-}
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  loading: true,
+  user: null
+};
 
-export default function (state = initialState, action) {
-    
-    const {type, payload} = action
-    
-    switch(type) {
-        case USER_LOADED:
-            return {
-                ...state,
-                isAuthenticated: true,
-                loading: false,
-                user: payload
-            }
-        case LOGIN_SUCCES:
-        case REGISTER_SUCCES:
-            localStorage.setItem("token", payload.token);
-            return {
-                ...state, 
-                ...payload,
-                isAuthenticated: true,
-                loading: false
-            }
-        case REGISTER_FAIL:
-        case AUTH_ERROR:
-        case LOGIN_FAIL:    
-            localStorage.removeItem("token")
-            return {
-                ...state, 
-                token:null,
-                isAuthenticated: false,
-                loading: false
-            }
-        default:
-             return state
-    }
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
+    case LOGIN_SUCCES:
+    case REGISTER_SUCCES:
+      localStorage.setItem("token", payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGOUT:
+    case LOGIN_FAIL:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      };
+    default:
+      return state;
+  }
 }
