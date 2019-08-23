295a6f4bc4c13bb44335e44e912aeb9ee90fca48
Sergey Zadorozhniy <wiseacreserj@gmail.com>
2019-08-23 15:34:38 +0300

    Add PrivateRoute

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Fragment, useState } from "react";
+import PropTypes from "prop-types";
+
+const Dashboard = () => {
+    return (
+        <div>
+            Dashboard
+        </div>
+    )
+}
+
+Dashboard.propTypes = {
+
+}
+
+export default Dashboard
